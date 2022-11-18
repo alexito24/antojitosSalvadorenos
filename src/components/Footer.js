90d1ad345430205e3004cba87533c02bde1b13c9
Heriberto lopez <alexito2002.ll@gmail.com>
2022-11-17 21:34:16 -0700

    changed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,34 +4,123 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PlaceIcon from "@mui/icons-material/Place";
+import Accordion from "react-bootstrap/Accordion";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import PhoneIcon from "@mui/icons-material/Phone";
+import DirectionsIcon from "@mui/icons-material/Directions";
+
+// import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export function Footer() {
   return (
     <footer className="footer">
       <Container className="footer_container">
+        <hr />
         <div className="footer_top">
           <div>
             <h4 className="footer_title">Antojitos Salvadoreño Restaurante</h4>
             <ul className="footer_list">
               <Container>
                 <Row>
-                  <Col className="textCenter">
-                    <h4>
-                      <AccessTimeIcon /> Working Hours
-                    </h4>
+                  <Col className="textEnd">
+                    <a className="link " href="tel:(303) 997-4295">
+                      <PhoneIcon />
+                      LLAMAR
+                    </a>
+                  </Col>
+                  <Col>
+                    <a
+                      className="link"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="Get directions to this location on Google Maps."
+                      href="https://goo.gl/maps/Z63CsKbLi7dCarNY8"
+                    >
+                      <DirectionsIcon />
+                      COMO LLEGAR
+                    </a>
                   </Col>
                 </Row>
                 <Row>
-                  <Col className="textEnd">Lunes a Viernes</Col>
-                  <Col>9:00am - 8:30pm</Col>
+                  <Col className="textCenter">
+                    <address>
+                      <a
+                        className="link"
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="Get directions to this location on Google Maps."
+                        href="https://goo.gl/maps/Z63CsKbLi7dCarNY8"
+                      >
+                        <PlaceIcon />
+                        3896 Morrison Rd, Denver, CO 80219
+                      </a>
+                    </address>
+                  </Col>
                 </Row>
+                <hr />
+                <Accordion className="textCenter" defaultActiveKey="0">
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                      <h4>
+                        <AccessTimeIcon /> Horarios
+                      </h4>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <br />
+                      <Row>
+                        <Col className="textEnd">Lunes a Viernes</Col>
+                        <Col>9:00am - 8:30pm</Col>
+                      </Row>
+                      <Row>
+                        <Col className="textEnd">Sábado</Col>
+                        <Col>9:00am - 9:00pm</Col>
+                      </Row>
+                      <Row>
+                        <Col className="textEnd">Domingo</Col>
+                        <Col>9:00am - 8:00pm</Col>
+                      </Row>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+                {/* <Accordion className="noStyle textCenter">
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    className="textCenter"
+                  >
+                    <Row className="textCenter">
+                      <Col>Horario</Col>
+                    </Row>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <br />
+                    <Row>
+                      <Col className="textEnd">Lunes a Viernes</Col>
+                      <Col>9:00am - 8:30pm</Col>
+                    </Row>
+                    <Row>
+                      <Col className="textEnd">Sábado</Col>
+                      <Col>9:00am - 9:00pm</Col>
+                    </Row>
+                    <Row>
+                      <Col className="textEnd">Domingo</Col>
+                      <Col>9:00am - 8:00pm</Col>
+                    </Row>
+                  </AccordionDetails>
+                </Accordion> */}
+                <hr />
                 <Row>
-                  <Col className="textEnd">Sábado</Col>
-                  <Col>9:00am - 9:00pm</Col>
-                </Row>
-                <Row>
-                  <Col className="textEnd">Domingo</Col>
-                  <Col>9:00am - 8:00pm</Col>
+                  <Col className="textCenter">
+                    <a className="link " href="/menu">
+                      <RestaurantMenuIcon /> Menú
+                    </a>
+                  </Col>
                 </Row>
               </Container>
             </ul>
